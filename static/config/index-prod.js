636f4bde6b7db7c1daaac6b7670e303af56b6f1e
yanwenyy <18738390058@163.com>
2020-11-10 16:16:22 +0800

    备份

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -5,8 +5,8 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://27.188.74.31:9080/jinding-back/';
-  window.SITE_CONFIG['wsUrl'] = 'ws://27.188.74.31:9080/jinding-back/imserver/';
+  window.SITE_CONFIG['baseUrl'] = 'http://222.222.237.187:9080/jinding-back/';
+  window.SITE_CONFIG['wsUrl'] = 'ws://222.222.237.187:9080/jinding-back/imserver/';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
